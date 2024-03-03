45a3174640dec6883fc0f340428713093dcda50c
iboutadg <iboutadg@student.1337.ma>
2024-03-03 12:38:29 +0100

    first commit

---
+++ ex00/Hello.py
@@ -0,0 +1,17 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello": "titi!"}
+
+ft_list[1] = "World"
+ft_tuple = ("Hello", "Morocco")
+ft_set.remove("tutu!")
+ft_set.remove("Hello")
+ft_set.add("Hello")
+ft_set.add("Ben Guerir")
+ft_dict["Hello"] = "1337"
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
---
+++ ex01/format_ft_time.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+print("Seconds since January 1, 1970:",end=" ")
+print("{:,.4f}".format(datetime.timestamp(datetime.now())), end=" or ")
+print("{:.2e}".format(datetime.timestamp(datetime.now())), end="")
+print(" in scientific notation")
+date = datetime.now()
+formatted = date.strftime("%b %d %Y")
+print(formatted)

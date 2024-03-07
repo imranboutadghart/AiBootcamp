721bfea38f58d904c0e942a857b6b41bdc97fd7c
iboutadg <iboutadg@student.1337.ma>
2024-03-07 15:52:29 +0100

    idek

---
+++ 01/ex00/give_bmi.py
@@ -0,0 +1,7 @@
+import numpy
+
+
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+	print()
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
